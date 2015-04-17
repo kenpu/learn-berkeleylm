94661ff680d1d80d44821495fb804aa5e0915775
Ken Pu <kenpuca@gmail.com>
2015-04-17 00:08:31 -0400

    initial creation with berkeleylm-1.1.5

---
+++ clm/project.clj
@@ -0,0 +1,8 @@
+(defproject clm "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :repositories {"local" ~(str (.toURI (java.io.File. "local_mvn_repo")))}
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [berkeleylm/berkeleylm "1.1.5"]])
---
+++ clm/test/clm/core_test.clj
@@ -0,0 +1,7 @@
+(ns clm.core-test
+  (:require [clojure.test :refer :all]
+            [clm.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
