c61ee2f73914da5442674c2844d1706b94e04128
Ken Pu <kenpu@mail.cs.toronto.edu>
2015-04-17 00:59:50 -0400

    wrapper functions around getLogProb

--- clm/src/clm/core.clj
+++ clm/src/clm/core.clj
@@ -1,6 +1,27 @@
-(ns clm.core)
+(ns clm.core
+  (:import (edu.berkeley.nlp.lm.io LmReaders))
+  (:require [clojure.string :as string]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn load-goog-lm []
+  (let [modeldir (System/getenv "LMDIR")
+        vocab (str modeldir "/vocab_cs.gz")
+        lm    (str modeldir "/eng.blm")]
+    (if (empty? modeldir)
+      (-> "LMDIR is not set" Exception. throw)
+      (LmReaders/readGoogleLmBinary lm vocab))))
+
+(defmulti logprob (fn [lm x] (class x)) :default nil)
+
+(defmethod logprob 
+  java.lang.String
+  [lm sentence]
+  (let [array (-> sentence (string/split #"\s+") java.util.ArrayList.)]
+    (logprob lm array)))
+
+(defmethod logprob
+  java.util.List
+  [lm sentence]
+  (.getLogProb lm sentence))
+
+(defn logprob
+
